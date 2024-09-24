c08d289c08beb10642db58d8ce3a7ef8586786f2
Italo <itaconsantos@gmail.com>
2024-09-23 21:46:19 -0300

    feature: product list page

--- app/(pages)/products/list/page.tsx
+++ app/(pages)/products/list/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
+import { Table } from "@/app/components/molecules/table";
+import { useProduct } from "@/app/hooks/useProduct";
+import { Product } from "./types";
+import { ROWS_PER_PAGE } from "@/app/utils/constants";
+import { Column } from "@/app/components/molecules/table/types";
 
 export default function ProductListPage() {
+  const { products } = useProduct();
+
+  const columns: Column<Product>[] = [
+    { key: "id", header: "ID" },
+    { key: "title", header: "Title" },
+    { key: "price", header: "Price" },
+    { key: "description", header: "Description" },
+    {
+      key: "image",
+      header: "Image",
+      render: (product: Product) => (
+        <Image src={product.image} alt={product.title} width="50" height="50" />
+      ),
+    },
+  ];
+
   return (
     <div>
       <h1>Products List</h1>
+      <Table data={products} columns={columns} rowsPerPage={ROWS_PER_PAGE} />
     </div>
   );
 }
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = "https://fakestoreapi.com/products";
+export const ROWS_PER_PAGE = 5;
